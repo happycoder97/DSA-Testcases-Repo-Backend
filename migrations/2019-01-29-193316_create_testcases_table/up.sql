bd1da820bc89c39d984b32ab3104e64fa0464044
Farzeen <happycoder97@protonmail.com>
2019-02-03 21:26:06 +0530

    added subject_id to testcases table

--- migrations/2019-01-29-193316_create_testcases_table/up.sql
+++ migrations/2019-01-29-193316_create_testcases_table/up.sql
@@ -2,6 +2,7 @@
 CREATE TABLE testcases(
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL, 
+    subject_id INTEGER NOT NULL,
     assg CHAR(5) NOT NULL,
     ques CHAR(5) NOT NULL,
     content TEXT NOT NULL
