34c917465bea894060a5785ecdd2f82768c0d8cb
Farzeen <happycoder97@protonmail.com>
2019-02-16 10:42:36 +0530

    New macro for diesel structs. Changed field names to match columns.

--- src/database/models.rs
+++ src/database/models.rs
@@ -1,70 +1,55 @@
 use super::schema::*;
 use uuid::Uuid;
 
-#[derive(Queryable)]
-pub struct User {
-    pub id: i32,
-    pub username: String,
-    pub password: String,
-    pub is_admin: bool,
+macro_rules! diesel_struct {
+    (
+        $(
+            pub struct $struct_name:ident, $new_struct_name: ident [$table_name: literal]{
+                $(pub $field:ident : $type:ty,)*
+            }
+        )+
+    ) => (
+        $(
+            #[derive(Queryable)]
+            #[table_name = $table_name]
+            pub struct $struct_name {
+                pub id: i32,
+                $(pub $field: $type),*
+            }
+
+            #[derive(Insertable)]
+            #[table_name = $table_name]
+            pub struct $new_struct_name {
+                $(pub $field: $type),*
+            }
+        )+
+    )
 }
 
-#[derive(Insertable)]
-#[table_name = "users"]
-pub struct NewUser {
-    pub username: String,
-    pub password: String,
-    pub is_admin: bool,
-}
+diesel_struct! {
+    pub struct User, NewUser ["users"] {
+        pub username: String,
+        pub password: String,
+        pub is_admin: bool,
+    }
 
-#[derive(Queryable)]
-pub struct Subject {
-    pub id: i32,
-    pub title: String,
-}
+    pub struct Subject, NewSubject ["subjects"] {
+        pub title: String,
+    }
 
-#[derive(Insertable)]
-#[table_name = "subjects"]
-pub struct NewSubject {
-    pub title: String,
-}
+    pub struct Testcase, NewTestcase ["testcases"] {
+        pub user_id: i32,
+        pub subject_id: i32,
+        pub assignment_char: String,
+        pub question_num: i32,
+        pub content: String,
+    }
 
-#[derive(Queryable)]
-pub struct Testcase {
-    pub id: i32,
-    pub user_id: i32,
-    pub subject_id: i32,
-    pub assg: String,
-    pub ques: String,
-    pub content: String,
-}
-
-#[derive(Insertable)]
-#[table_name = "testcases"]
-pub struct NewTestcase {
-    pub user_id: i32,
-    pub subject_id: i32,
-    pub assg: String,
-    pub ques: String,
-    pub content: String,
-}
-
-#[derive(Queryable)]
-pub struct Submission {
-    pub id: i32,
-    pub user_id: i32,
-    pub testcase_id: i32,
-    pub content: String,
-    pub hash1: Uuid,
-    pub hash2: Uuid,
-}
-
-#[derive(Insertable)]
-#[table_name = "submissions"]
-pub struct NewSubmission {
-    pub user_id: i32,
-    pub testcase_id: i32,
-    pub content: String,
-    pub hash1: Uuid,
-    pub hash2: Uuid,
+    pub struct Submission, NewSubmission ["submissions"] {
+        pub user_id: i32,
+        pub testcase_id: i32,
+        pub content: String,
+        pub hash1: Uuid,
+        pub hash2: Uuid,
+    }
 }
